cc90b7a7917b5fb3cfd200dd1e94002d3a9ca4fe
madhur pare <paremadhur5@gmail.com>
2025-08-12 15:50:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from './assets/components/Navbar'
+
+import './App.css'
+import Aboutus from './assets/components/Aboutus'
+// import About from ".//About Us";
+
+
+const App = ()=> {
+ 
+
+  return (
+    <div className='container' >
+     <Navbar/>
+      <Aboutus/>
+   
+    </div>
+    
+  )
+}
+
+export default App
